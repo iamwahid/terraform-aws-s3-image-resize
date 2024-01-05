ca946e64e43ec460d832e702891f53f9401df224
Wahid Niam <iam.wahidn@gmail.com>
2024-01-05 15:39:22 +0700

    feat: update nodejs

--- aws_lambda.tf
+++ aws_lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "img_resize" {
   handler          = "index.handler"
   filename         = data.archive_file.img_resize_zip.output_path
   source_code_hash = data.archive_file.img_resize_zip.output_base64sha256
-  runtime          = "nodejs12.x"
+  runtime          = "nodejs18.x"
   timeout          = "60"
   layers           = var.image_magick_layers
 }
