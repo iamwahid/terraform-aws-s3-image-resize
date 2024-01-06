a9eba7125135d2425695421017b7af3c56aa5866
Wahid Niam <iam.wahidn@gmail.com>
2024-01-06 11:22:19 +0700

    fix: use imagemagick nested legacy 5

--- lambda/lib/ImageResizer.js
+++ lambda/lib/ImageResizer.js
@@ -68,7 +68,7 @@ class ImageResizer {
                     if (h < n) {
                         n = h;
                     }
-                    img = gm(image.data).geometry(`${n}x${n}`).gravity('center').crop(n, n);
+                    img = gm(image.data).setFormat('jpeg').geometry(`${n}x${n}^`).gravity('center').extent(n, n);
                     console.log(img)
 
                     const fname = image.fileName.split('/')[image.fileName.split('/').length - 1];
